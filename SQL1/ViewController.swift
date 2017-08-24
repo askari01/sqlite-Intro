0c4c8f1db6b005aa70bcbcf07ddb4ed9958d5a4a
askari01 <farrukh.askari01@gmail.com>
2017-08-24 14:56:16 +0500

    Initial Commit

---
+++ SQL1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQL1
+//
+//  Created by Syed Askari on 8/24/17.
+//  Copyright © 2017 Syed Askari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
